3d21311ea67ddf1f10937c2fd7f3b23c79979b8b
Rifkyy29 <149021794+Rifkyy29@users.noreply.github.com>
2024-12-15 00:18:28 +0800

    ketinggalan

---
+++ src/Behavior.java
@@ -0,0 +1,5 @@
+public interface Behavior {
+    void move();
+
+    void shoot();
+}
---
+++ src/GameObject.java
@@ -0,0 +1,55 @@
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public abstract class GameObject {
+    protected int x, y, width, height;
+    private BufferedImage image;;
+
+    public GameObject(int x, int y, int width, int height, BufferedImage image) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
